fc66c8d63cdf1621467a8d13c23603394441b5de
Kevin Gao <kevinzhugao@gmail.com>
2021-11-13 19:53:00 -0800

    Created basic outline of server

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const PORT = 8000;
+
+app.post('/history', (req, res) => {
+});
+
+app.get('/history', (req, res) => {
+});
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
